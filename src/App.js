ead8edd814a31ee988385026a740e72b633c93be
rabiul155 <siyamict@gmail.com>
2022-10-25 10:49:18 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Home from './component/Home/Home';
+import Main from './layout/Main';
+
+
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element: <Home></Home>
+        }
+
+      ]
+    }
+  ])
+
+
   return (
+
+
     <div className="App">
+
+      <RouterProvider router={router}></RouterProvider>
 
     </div>
   );
