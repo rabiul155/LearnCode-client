758f191c7dd107d97f00c39606e67c22c138b4bd
rabiul155 <siyamict@gmail.com>
2022-12-22 15:59:26 +0600

    ui updated

--- src/component/Footer/Footer.js
+++ src/component/Footer/Footer.js
@@ -3,8 +3,8 @@ import './Footer.css'
 
 const Footer = () => {
     return (
-        <div>
-            <footer class="bg-light text-center text-lg-start">
+        <div className=' pt-4'>
+            <footer class="bg-light text-center text-lg-start ">
 
                 <div className="text-center  p-3 footer-color">
                     © 2020 Copyright : LearnCode.com
