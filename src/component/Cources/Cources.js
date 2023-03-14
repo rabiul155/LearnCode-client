23bd3ec8688010564f8719b5437d92a9a870d122
rabiul155 <siyamict@gmail.com>
2023-03-14 16:14:53 +0600

    subscription add

--- src/component/Cources/Cources.js
+++ src/component/Cources/Cources.js
@@ -5,6 +5,11 @@ import CourceCard from '../CourseCard/CourceCard';
 import Loading from '../Loading/Loading';
 import './Cources.css'
 import img from '../../images/feedback.webp'
+import cardWave1 from '../../images/blue-wave.svg'
+import cardWave2 from '../../images/brown-wave.svg'
+import sub1 from '../../images/dis1.webp'
+import sub2 from '../../images/dis2.webp'
+import sub3 from '../../images/dis3.webp'
 
 const Cources = () => {
 
@@ -100,6 +105,49 @@ const Cources = () => {
                     </div>
 
                 </div>
+            </section>
+
+            <section className='m-4'>
+
+                <div className='row  row-cols-1 row-cols-md-2 row-cols-lg-3'>
+                    <div class="col px-0 mx-auto card card-details position-relative">
+                        <div class="card-body py-4">
+                            <h4 class="card-title w-50 " >Get 3 month subscription</h4>
+                            <p class="card-text ">Prouductivity course</p>
+                        </div>
+                        <img src={cardWave1} class="card-img-bottom" alt="..." />
+                        <img className='subImg img-fluid position-absolute bottom-0 end-0' src={sub1} alt="" />
+                        <div className='discount position-absolute  py-1'>
+                            <p className='text-center mb-0 '>Upto 20% off</p>
+                            <h1 className=' text-center'>20%</h1>
+                        </div>
+                    </div>
+                    <div class="col  px-0 mx-auto card card-details position-relative">
+                        <div class="card-body py-4">
+                            <h4 class="card-title w-50 " >Get 6 month subscription</h4>
+                            <p class="card-text ">Prouductivity course</p>
+                        </div>
+                        <img src={cardWave2} class="card-img-bottom" alt="..." />
+                        <img className='subImg  position-absolute bottom-0 end-0' src={sub3} alt="" />
+                        <div className='discount position-absolute  py-1'>
+                            <p className='text-center mb-0 '>Upto 30% off</p>
+                            <h1 className=' text-center'>30%</h1>
+                        </div>
+                    </div>
+                    <div class="col px-0 mx-auto card card-details position-relative">
+                        <div class="card-body">
+                            <h4 class="card-title w-50 " >Get 9 month subscription</h4>
+                            <p class="card-text ">Prouductivity course</p>
+                        </div>
+                        <img src={cardWave1} class="card-img-bottom " alt="..." />
+                        <img className='subImg lastImg position-absolute bottom-0 end-0' src={sub2} alt="" />
+                        <div className='discount position-absolute  py-1'>
+                            <p className='text-center mb-0 '>Upto 50% off</p>
+                            <h1 className=' text-center'>50%</h1>
+                        </div>
+                    </div>
+                </div>
+
             </section>
 
         </div>
